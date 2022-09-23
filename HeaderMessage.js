821f0b8336c119ff343748b9c32ffecedf24b4bd
Marc Nicdao <marc.nicdao@accenture.com>
2022-09-23 14:05:21 -0700

    refactored HeaderMessage

--- HeaderMessage.js
+++ HeaderMessage.js
@@ -1,34 +1,29 @@
 export default class HeaderMessage {
-  start() {
-    console.log("started");
-    const messages = [
+  constructor() {
+    this.messages = [
       "Software Engineer    ",
       "Lifetime Learner    ",
       "Thinker     ",
     ];
+    this.text = document.querySelector(".header__message");
+  }
 
-    let hello = document.querySelector(".header__message");
-    hello.innerText = "";
-    const writeMessage = async function (messages) {
-      let index = 0;
-      while (true) {
-        for (let i in messages[index]) {
-          await new Promise((resolve) =>
-            setTimeout(resolve, (2 / messages[index].length) * 1000)
-          ).then(() => {
-            console.log("jm");
-            hello.innerText = messages[index].substring(0, i);
-          });
-        }
-        for (let j = messages[index].length - 1; j >= 0; j--) {
-          await new Promise((resolve) => setTimeout(resolve, 50)).then(() => {
-            hello.innerText = messages[index].substring(0, j);
-          });
-        }
-        index = index + 1 === messages.length ? 0 : index + 1;
+  async start() {
+    let index = 0;
+    while (true) {
+      for (let i in this.messages[index]) {
+        await new Promise((resolve) =>
+          setTimeout(resolve, (2 / this.messages[index].length) * 1000)
+        ).then(() => {
+          this.text.innerText = this.messages[index].substring(0, i);
+        });
       }
-    };
-
-    writeMessage(messages);
+      for (let j = this.messages[index].length - 1; j >= 0; j--) {
+        await new Promise((resolve) => setTimeout(resolve, 50)).then(() => {
+          this.text.innerText = this.messages[index].substring(0, j);
+        });
+      }
+      index = index + 1 === this.messages.length ? 0 : index + 1;
+    }
   }
 }
